24a2b151b554aaa853a1089ee106dc28e1bed069
Marko Perica <maperica@vsite.hr>
2025-04-15 15:02:22 +0200

    Added navigation integration with Shell.

--- DostavniPotpis/App.xaml.cs
+++ DostavniPotpis/App.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace DostavniPotpis
+﻿using DostavniPotpis.Services.Navigation;
+
+namespace DostavniPotpis
 {
     public partial class App : Application
     {
-        public App()
+        public App(INavigationService navigationService)
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new AppShell(navigationService);
         }
     }
 }
--- DostavniPotpis/AppShell.xaml.cs
+++ DostavniPotpis/AppShell.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace DostavniPotpis
+﻿using DostavniPotpis.Services.Navigation;
+using DostavniPotpis.Views;
+
+namespace DostavniPotpis
 {
     public partial class AppShell : Shell
     {
-        public AppShell()
+        private readonly INavigationService _navigationService;
+        public AppShell(INavigationService navigationService)
         {
+            _navigationService = navigationService;
+
+            AppShell.InitializeRouting();
+
             InitializeComponent();
+        }
+
+        private static void InitializeRouting()
+        {
+            Routing.RegisterRoute("SignatureView", typeof(SignatureView));
+            Routing.RegisterRoute("MainView", typeof(MainView));
+            Routing.RegisterRoute("TransferView", typeof(TransferView));            
         }
     }
 }
